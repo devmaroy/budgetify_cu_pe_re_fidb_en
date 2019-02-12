12f01ba64a3810575785b37b660400e00d0d586c
Marek Matejovic <me@devmaroy.cz>
2019-02-12 12:04:24 +0100

    Styles for form error

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -66,7 +66,7 @@ export default class ExpenseForm extends React.Component {
     render() {
         return (
             <form className="form" onSubmit={ this.onSubmit }>
-                { this.state.error && <p>{ this.state.error }</p> }
+                { this.state.error && <p className="form__error">{ this.state.error }</p> }
                 <input 
                     type="text"
                     placeholder="Description"
